0adac3a18f0ed40f31f261cf3805c487ef881bdb
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2024-11-27 16:58:50 -0500

    Fix navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+	const [isMenuVisible, setIsMenuVisible] = useState(false)
+
+	const toggleMenu = () => {
+		setIsMenuVisible(!isMenuVisible)
+	}
+
 	return (
 		<>
 			<header id="header" className="">
 				<h1><a href="/">Blu-Moon Driving School</a></h1>
-
 				<nav>
-					<NavLink to="/">Home</NavLink>
-					<NavLink to="/services">Services</NavLink>
-					<NavLink to="/resources">Resources</NavLink>
-					<NavLink to="/faq">FAQ</NavLink>
+					<a href="#" className="small" onClick={toggleMenu}>Menu</a>
 				</nav>
-				
+				{isMenuVisible ?
+					<div className="modal">
+						<div className="modal-content" onClick={toggleMenu}>
+							<div>
+								<NavLink to="/" onClick={toggleMenu}>Home</NavLink><br />
+								<NavLink to="/services" onClick={toggleMenu}>Services</NavLink><br />
+								<NavLink to="/resources" onClick={toggleMenu}>Resources</NavLink><br />
+								<NavLink to="/faq" onClick={toggleMenu}>FAQ</NavLink>
+							</div>
+							<button className="small" onClick={toggleMenu}>Close</button>
+						</div>
+					</div>
+					:
+					''
+				}
 			</header>
 
 		</>

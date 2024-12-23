a0875b01a43bce9e00e1343dfa19db0274e44dda
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2024-12-23 09:17:57 -0500

    Hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import App from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )

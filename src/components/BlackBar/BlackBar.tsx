d4d34a558d6db8be4a8e396699179505784ee8dd
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2024-12-28 14:09:12 -0500

    Mobile text optimize

--- src/components/BlackBar/BlackBar.tsx
+++ src/components/BlackBar/BlackBar.tsx
@@ -1,17 +1,21 @@
+import { useMediaQuery } from 'usehooks-ts'
 import './blackbar.css'
 
 export default function BlackBar() {
+    
+  // You can use any @media property
+    const isDesktop: boolean = useMediaQuery('(min-width: 800px)');
     return (
         <div className='blackBar'>
             <div className='blackBarRow'>
                 <a href="tel://14432760761/">
-                    <span className="icon solid fa-phone"> (443) 276-0761</span>
+                    <span className="icon solid fa-phone"> {isDesktop ? '(443) 276-0761' : '' }</span>
                 </a>
                 <a href="https://maps.app.goo.gl/1LRSuYzvnpQth5T19" target="_blank">
-                    <span className="icon solid fa-map"> 8950 Old Annapolis Road #115, Columbia, MD 21045</span>
+                    <span className="icon solid fa-map"> {isDesktop ? '8950 Old Annapolis Road #115, Columbia, MD 21045' : '' }</span>
                 </a>
                 <a href="mailto:blumoondriving@gmail.com">
-                    <span className="icon solid fa-envelope"> blumoondriving@gmail.com</span>
+                    <span className="icon solid fa-envelope">{isDesktop ? 'blumoondriving@gmail.com' : ''} </span>
                 </a>
             </div>
         </div>

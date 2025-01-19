a89b50b37607a5b354e269027d5d7f8d7e5e68fa
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2025-01-18 23:03:50 -0500

    Add unit one

--- src/components/Quiz/allQuestions.ts
+++ src/components/Quiz/allQuestions.ts
@@ -6,25 +6,113 @@ export default function AllQuestions() {
             "questionsAtOnce": 1,
             "quiz": [
                 {
-                    "question": "This is sample question one for unit one. Correct answer is D",
+                    "question": "When is a provisional license holder under 18 not allowed to drive ____ without special permissions?",
                     "image": null,
-                    "correctAnswer": "D",
+                    "correctAnswer": "B",
                     "answers": [
-                        { "key": "A", "item": "Answer A" },
-                        { "key": "B", "item": "Answer B" },
-                        { "key": "C", "item": "Answer C" },
-                        { "key": "D", "item": "Answer D" }
+                        { "key": "A", "item": "between 8:00 a.m. and 9:00 p.m." },
+                        { "key": "B", "item": "between 12:00 a.m. and 5:00 a.m." },
+                        { "key": "C", "item": "between 5:01 a.m. and 11:59 p.m." },
+                        { "key": "D", "item": "between 6:00 a.m. and 6:00 p.m." }
                     ]
                 },
                 {
-                    "question": "This is sample question two for unit one. Correct answer is C",
+                    "question": "While driving, a new driver under 18 is prohibited from ___",
+                    "image": null,
+                    "correctAnswer": "D",
+                    "answers": [
+                        { "key": "A", "item": "using a cell phone to call family." },
+                        { "key": "B", "item": "texting a family member." },
+                        { "key": "C", "item": "having a BAC of .00 or greater." },
+                        { "key": "D", "item": "all of the above" }
+                    ]
+                },
+                {
+                    "question": "If a learner's permit holder commits a moving violation, he/she",
+                    "image": null,
+                    "correctAnswer": "A",
+                    "answers": [
+                        { "key": "A", "item": "must restart the 9-month waiting period to receive a provisional license." },
+                        { "key": "B", "item": "must complete the Driver Improvement Program on line" },
+                        { "key": "C", "item": "must wait until he/she is 21 to receive a driver's license." },
+                        { "key": "D", "item": "all of the above." }
+                    ]
+                },
+                {
+                    "question": "During the first 5 months (151 days) of the provisional period, provisional license holders under the age of 18",
+                    "image": null,
+                    "correctAnswer": "A",
+                    "answers": [
+                        { "key": "A", "item": "must not have passengers under the age of 18, unless accompanied by a qualified supervising driver or unless the passengers are direct family members." },
+                        { "key": "B", "item": "must have all passengers in the front seat." },
+                        { "key": "C", "item": "may have as many passengers as will fit into their vehicles." },
+                        { "key": "D", "item": "may only have passengers during daytime hours." }
+                    ]
+                },
+                {
+                    "question": "Once a parent/guardian signs for a provisional license,",
+                    "image": null,
+                    "correctAnswer": "B",
+                    "answers": [
+                        { "key": "A", "item": "it cannot be taken away." },
+                        { "key": "B", "item": "the license may be cancelled by written request from the parent or guardian." },
+                        { "key": "C", "item": "he/she will receive points on his/her driver's license if the new driver commits an offense." },
+                        { "key": "D", "item": "none of the above." }
+                    ]
+                },
+                {
+                    "question": "A new driver under the age of 25 is required by law to practice night driving for at least ___ hours.",
                     "image": null,
                     "correctAnswer": "C",
                     "answers": [
-                        { "key": "A", "item": "Answer A" },
-                        { "key": "B", "item": "Answer B" },
-                        { "key": "C", "item": "Answer C" },
-                        { "key": "D", "item": "Answer D" }
+                        { "key": "A", "item": "6 hours" },
+                        { "key": "B", "item": "8 hours" },
+                        { "key": "C", "item": "10 hours" },
+                        { "key": "D", "item": "12 hours" }
+                    ]
+                },
+                {
+                    "question": "A person who has a Learner's Instructional Permit",
+                    "image": null,
+                    "correctAnswer": "C",
+                    "answers": [
+                        { "key": "A", "item": "may drive with the supervision of any licensed driver." },
+                        { "key": "B", "item": "may drive without someone supervising." },
+                        { "key": "C", "item": "must drive with a person 21 or older who has held a license for at least 3 years." },
+                        { "key": "D", "item": "must drive with a person 18 or older who has held a license for at least 3 years." }
+                    ]
+                },
+                {
+                    "question": "For drivers under age 25, ___ hours of practice must be recorded in the Practice Skills Log.",
+                    "image": null,
+                    "correctAnswer": "A",
+                    "answers": [
+                        { "key": "A", "item": "60" },
+                        { "key": "B", "item": "50" },
+                        { "key": "C", "item": "40" },
+                        { "key": "D", "item": "30" }
+                    ]
+                },
+                {
+                    "question": "When going to the MVA to take your skills test, you must bring",
+                    "image": null,
+                    "correctAnswer": "D",
+                    "answers": [
+                        { "key": "A", "item": "a clean, well maintained vehicle." },
+                        { "key": "B", "item": "registration and proof of insurance." },
+                        { "key": "C", "item": "a supervising driver." },
+                        { "key": "D", "item": "all of the above." }
+                    ]
+                },
+                {
+                    "question": "To obtain a provisional license in Maryland,",
+                    "image": null,
+                    "correctAnswer": "A",
+                    "answers": [
+                        { "key": "A", "item": "a new driver must pass a skills test including a closed course and an open road test." },
+                        { "key": "B", "item": "a new driver must be registered for driver education" },
+                        { "key": "C", "item": "a new driver must have held a learner's instructional permit for 18 months." },
+                        { "key": "D", "item": "a new driver must successfully complete 40 hours of practice driving." }
                     ]
                 }
             ],
